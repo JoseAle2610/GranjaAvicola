76cd25bdce203cd75e04b6f81c3719c7f34c5be2
Azumie <azumie250@gmail.com>
2020-11-26 13:11:23 -0300

    Signed-off-by: Azumie <azumie250@gmail.com>

--- sql/granjaavicolalastunas (1).sql
+++ sql/granjaavicolalastunas (1).sql
@@ -170,18 +170,19 @@ CREATE TABLE `registros` (
 --
 
 CREATE TABLE `responsables` (
-  `idResponsable` int(11) NOT NULL,
   `nombreResponsable` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `apellidoResponsable` varchar(45) COLLATE utf8_spanish_ci NOT NULL
+  `apellidoResponsable` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `activo`               tinyint(1) not null DEFAULT 1,
+  `ci`               varchar(10) not null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `responsables`
 --
 
-INSERT INTO `responsables` (`idResponsable`, `nombreResponsable`, `apellidoResponsable`) VALUES
-(1, 'Jose Alexander', 'Suarez Gervazzi'),
-(2, 'Antonella', 'Mujica');
+INSERT INTO `responsables` (`ci`, `nombreResponsable`, `apellidoResponsable`) VALUES
+("29587834", 'Jose Alexander', 'Suarez Gervazzi'),
+("27554995", 'Antonella', 'Mujica');
 
 -- --------------------------------------------------------
 
@@ -191,7 +192,7 @@ INSERT INTO `responsables` (`idResponsable`, `nombreResponsable`, `apellidoRespo
 
 CREATE TABLE `responsablesderegistro` (
   `idRegistro` int(11) NOT NULL,
-  `idResponsable` int(11) NOT NULL
+  `ci` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -226,16 +227,15 @@ CREATE TABLE `usuarios` (
   `nombreUsuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `claveUsuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `fechaCreacion` date NOT NULL,
-  `activo` tinyint(1) NOT NULL DEFAULT '1'
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `pregunta`         varchar(30) NOT NULL,
+  `respuesta`        varchar(30) NOT NULL,
+  `ci`               varchar(10) not null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Volcado de datos para la tabla `usuarios`
---
 
-INSERT INTO `usuarios` (`idUsuarios`, `nombreUsuario`, `claveUsuario`, `fechaCreacion`, `activo`) VALUES
-(1, 'Admin', '123456', '2020-09-16', 1);
-
+INSERT INTO `usuarios` (`idUsuarios`, `nombreUsuario`, `claveUsuario`, `fechaCreacion`, `activo`, `pregunta`, `respuesta`, `ci`) 
+VALUES (NULL, 'Admin', '123456', '2020-11-26', '1', '¿Sakura o Hinata?', 'Sakura', '27554995');
 -- --------------------------------------------------------
 
 --
@@ -304,14 +304,15 @@ ALTER TABLE `registros`
 -- Indices de la tabla `responsables`
 --
 ALTER TABLE `responsables`
-  ADD PRIMARY KEY (`idResponsable`);
+  ADD PRIMARY KEY (`ci`);
+
 
 --
 -- Indices de la tabla `responsablesderegistro`
 --
 ALTER TABLE `responsablesderegistro`
-  ADD PRIMARY KEY (`idRegistro`,`idResponsable`),
-  ADD KEY `fk_Registro_has_Responsables_Responsables1_idx` (`idResponsable`),
+  ADD PRIMARY KEY (`idRegistro`,`ci`),
+  ADD KEY `fk_Registro_has_Responsables_Responsables1_idx` (`ci`),
   ADD KEY `fk_Registro_has_Responsables_Registro1_idx` (`idRegistro`);
 
 --
@@ -326,7 +327,8 @@ ALTER TABLE `sectores`
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`idUsuarios`);
+  ADD PRIMARY KEY (`idUsuarios`),
+  ADD KEY `fk_Usuarios_has_Responsables_Responsables1_idx` (`ci`);
 
 --
 -- Indices de la tabla `valores`
@@ -364,11 +366,6 @@ ALTER TABLE `recogidas`
 ALTER TABLE `registros`
   MODIFY `idRegistro` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `responsables`
---
-ALTER TABLE `responsables`
-  MODIFY `idResponsable` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `sectores`
@@ -390,47 +387,51 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `galponesenlote`
 --
 ALTER TABLE `galponesenlote`
-  ADD CONSTRAINT `fk_Galpones_has_Lotes_Galpones` FOREIGN KEY (`idGalpon`) REFERENCES `galpones` (`idGalpon`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Galpones_has_Lotes_Lotes1` FOREIGN KEY (`idLote`) REFERENCES `lotes` (`idLote`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Galpones_has_Lotes_Galpones` FOREIGN KEY (`idGalpon`) REFERENCES `galpones` (`idGalpon`) ON DELETE cascade ON UPDATE cascade,
+  ADD CONSTRAINT `fk_Galpones_has_Lotes_Lotes1` FOREIGN KEY (`idLote`) REFERENCES `lotes` (`idLote`) ON DELETE cascade ON UPDATE cascade;
 
 --
 -- Filtros para la tabla `mortalidad`
 --
 ALTER TABLE `mortalidad`
-  ADD CONSTRAINT `fk_Mortalidad_Galpones_has_Lotes1` FOREIGN KEY (`idGalpon`,`idLote`) REFERENCES `galponesenlote` (`idGalpon`, `idLote`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Mortalidad_Galpones_has_Lotes1` FOREIGN KEY (`idGalpon`,`idLote`) REFERENCES `galponesenlote` (`idGalpon`, `idLote`) ON DELETE cascade ON UPDATE cascade;
 
 --
 -- Filtros para la tabla `recogidas`
 --
 ALTER TABLE `recogidas`
-  ADD CONSTRAINT `fk_Recogidas_Registro1` FOREIGN KEY (`idRegistro`) REFERENCES `registros` (`idRegistro`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Recogidas_Registro1` FOREIGN KEY (`idRegistro`) REFERENCES `registros` (`idRegistro`) ON DELETE cascade ON UPDATE cascade;
 
 --
 -- Filtros para la tabla `registros`
 --
 ALTER TABLE `registros`
-  ADD CONSTRAINT `fk_Registro_Sectores1` FOREIGN KEY (`idSector`) REFERENCES `sectores` (`idSector`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Registro_Sectores1` FOREIGN KEY (`idSector`) REFERENCES `sectores` (`idSector`) ON DELETE cascade ON UPDATE cascade;
 
 --
 -- Filtros para la tabla `responsablesderegistro`
 --
 ALTER TABLE `responsablesderegistro`
-  ADD CONSTRAINT `fk_Registro_has_Responsables_Registro1` FOREIGN KEY (`idRegistro`) REFERENCES `registros` (`idRegistro`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Registro_has_Responsables_Responsables1` FOREIGN KEY (`idResponsable`) REFERENCES `responsables` (`idResponsable`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Registro_has_Responsables_Registro1` FOREIGN KEY (`idRegistro`) REFERENCES `registros` (`idRegistro`) ON DELETE cascade ON UPDATE cascade,
+  ADD CONSTRAINT `fk_Registro_has_Responsables_Responsables1` FOREIGN KEY (`ci`) REFERENCES `responsables` (`ci`) ON DELETE cascade ON UPDATE cascade;
 
 --
 -- Filtros para la tabla `sectores`
 --
 ALTER TABLE `sectores`
-  ADD CONSTRAINT `fk_Sectores_Galpones1` FOREIGN KEY (`idGalpon`) REFERENCES `galpones` (`idGalpon`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Sectores_Galpones1` FOREIGN KEY (`idGalpon`) REFERENCES `galpones` (`idGalpon`) ON DELETE cascade ON UPDATE cascade;
 
 --
 -- Filtros para la tabla `valores`
 --
 ALTER TABLE `valores`
-  ADD CONSTRAINT `fk_Recogidas_has_Categorias_Categorias1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Recogidas_has_Categorias_Recogidas1` FOREIGN KEY (`idRecogida`) REFERENCES `recogidas` (`idRecogida`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Recogidas_has_Categorias_Categorias1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`) ON DELETE cascade ON UPDATE cascade,
+  ADD CONSTRAINT `fk_Recogidas_has_Categorias_Recogidas1` FOREIGN KEY (`idRecogida`) REFERENCES `recogidas` (`idRecogida`) ON DELETE cascade ON UPDATE cascade;
 COMMIT;
+
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_Usuarios_has_Responsables_Usuarios1` FOREIGN KEY (`ci`) REFERENCES `responsables` (`ci`) ON DELETE cascade ON UPDATE cascade;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
